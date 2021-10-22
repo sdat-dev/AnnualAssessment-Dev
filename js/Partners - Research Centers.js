41c2c027f5e09a2a2b3ddd91965dd1dcf8fda792
Chirag Charania <chiragcharania@gmail.com>
2021-10-22 09:59:13 -0400

    Moved files to AutomationScripts

--- js/Partners - Research Centers.js
+++
@@ -1,9 +0,0 @@
-Partners = {};
-Partners["Year"] = entry.hasOwnProperty("field_year")? entry.field_year : "";
-Partners["Unit"] = entry.hasOwnProperty("field_Title")? entry.field_Title : "";
-Partners["partnerFullName"] = entry.hasOwnProperty("field_partnerFullName")? entry.field_partnerFullName : "";
-Partners["partnerJobTitle"] = entry.hasOwnProperty("field_partnerJobTitle")? entry.field_partnerJobTitle : "";
-Partners["partnerDepartment"] = entry.hasOwnProperty("field_partnerDepartment")? entry.field_partnerDepartment : "";
-Partners["partnerSchool"] = entry.hasOwnProperty("field_partnerSchool")? entry.field_partnerSchool : "";
-Partners["partnerOrganization"] = entry.hasOwnProperty("field_partnerOrganization")? entry.field_partnerOrganization : "";
-Partners["partnerEmail"] = entry.hasOwnProperty("field_partnerEmail")? entry.field_partnerEmail : "";
